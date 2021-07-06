d831e58ac60987ddbac7b9b6419b1c54a59333a8
Sam AntonY <saman2ny@outlook.com>
2021-07-07 00:04:38 +0530

    download and design

--- src/pages/survey/reports-survey/reports-survey.component.ts
+++ src/pages/survey/reports-survey/reports-survey.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit ,Input, ViewChildren, ViewChild,ElementRef, Renderer2} from '@angular/core';
+import { Component, OnInit ,Input, ViewChildren, ViewChild,ElementRef, Renderer2, HostListener} from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray, FormControl, ValidatorFn, AbstractControl } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
@@ -19,6 +19,7 @@ import jsPDF from 'jspdf';
 
 
 import html2canvas from 'html2canvas';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-reports-survey',
@@ -46,10 +47,14 @@ export class ReportsSurveyComponent implements OnInit {
 
 @ViewChild('chart')
 chart: GoogleChartComponent;
+@HostListener('window:resize', ['$event'])
 
   chartData: Array<any>;
  type: any = {};
  @ViewChild('content', {static: false}) content: ElementRef;
+
+
+
 
 
   constructor(private formBuilder: FormBuilder, private router: Router, public common: CommonService, private apiService: ApiService,
@@ -58,48 +63,175 @@ chart: GoogleChartComponent;
 
     }
 
-    downloadReport(){
-      const doc = new jsPDF();
-
-      doc.text("Hello world!", 10, 10);
-      doc.save("a4.pdf");
-
-    }
 
 
-
-    generarPDF() {
+    async  generarPDF() {
       this.common.showLoading()
-      const div = document.getElementById('content');
-      const options = {
-        background: 'white',
-        scale: 3
-      };
+      // const div = document.getElementById('content');
+      // const options = {
+      //   background: 'white',
+      //   scale: 3
+      // };
   
-      html2canvas(div, options).then((canvas) => {
+      // html2canvas(div, options).then((canvas) => {
   
-        var img = canvas.toDataURL("image/PNG");
-        var doc = new jsPDF('l', 'mm', 'a4', 1);
+      //   var img = canvas.toDataURL("image/PNG");
+      //   var doc = new jsPDF('p', 'mm', 'a4', 1);
   
-        // Add image Canvas to PDF
-        const bufferX = 5;
-        const bufferY = 5;
-        const imgProps = (<any>doc).getImageProperties(img);
-        const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
-        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-        doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
-  
-        return doc;
-      }).then((doc) => {
-        doc.save('postres.pdf');  
-        this.common.hideLoading()
+      //   // Add image Canvas to PDF
+      //   const bufferX = 5;
+      //   const bufferY = 5;
+      //   const imgProps = (<any>doc).getImageProperties(img);
+      //   const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      //   const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      //   doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
 
-      });
+      //   return doc;
+      // }).then((doc) => {
+      //   var d = new Date().toISOString().slice(0, 19).replace(/-/g, "");
+      //   var filename = 'report_' + d + '.pdf';
+      //   doc.save(filename);  
+      //   this.common.hideLoading()
+
+      // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // const div = document.getElementById('content');
+
+
+      // var HTML_Width = $("#content").width();
+      // var HTML_Height = $("#content").height();
+      // var top_left_margin = 15;
+      // var PDF_Width = HTML_Width+(top_left_margin*2);
+      // var PDF_Height = (PDF_Width*1.5)+(top_left_margin*2);
+      // var canvas_image_width = HTML_Width;
+      // var canvas_image_height = HTML_Height;
+      
+      // var totalPDFPages = Math.ceil(HTML_Height/PDF_Height)-1;
+      
+  
+      // html2canvas($("#content")[0],{allowTaint:true}).then(function(canvas) {
+      //   canvas.getContext('2d');
+        
+      //   console.log(canvas.height+"  "+canvas.width);
+        
+        
+      //   var imgData = canvas.toDataURL("image/jpeg", 1.0);
+      //   var pdf = new jsPDF('p', 'pt',  [PDF_Width, PDF_Height]);
+      //     pdf.addImage(imgData, 'PNG', top_left_margin, top_left_margin,canvas_image_width,canvas_image_height);
+        
+        
+      //   for (var i = 1; i <= totalPDFPages; i++) { 
+      //     pdf.addPage(PDF_Width, PDF_Height);
+      //     pdf.addImage(imgData, 'PNG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4),canvas_image_width,canvas_image_height);
+      //   }
+      //           var d = new Date().toISOString().slice(0, 19).replace(/-/g, "");
+      //   var filename = 'report_' + d + '.pdf';
+      //     pdf.save(filename);
+
+      //     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      //       const div = document.getElementById('content');
+      // const options = {
+      //   background: 'white',
+      //   scale: 3
+      // };
+  
+      // html2canvas(div, options).then((canvas) => {
+      //   console.log(div, "div")
+      //   var img = canvas.toDataURL("image/PNG");
+      //   var doc = new jsPDF('p', 'mm', 'a4');
+  
+      //   // Add image Canvas to PDF
+      //   const bufferX = 5;
+      //   const bufferY = 5;
+      //   const imgProps = (<any>doc).getImageProperties(img);
+      //   const pdfWidth = doc.internal.pageSize.getWidth() - 1 * bufferX;
+      //   const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      //   doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+
+      //   // return doc;
+      //   var d = new Date().toISOString().slice(0, 19).replace(/-/g, "");
+      //   var filename = 'report_' + d + '.pdf';
+      //   doc.save(filename);  
+      // })
+
+
+
+
+
+
+
+
+      const doc = new jsPDF('p', 'mm', 'a4');
+      const options = {
+        pagesplit: true
+      };
+      // const ids = document.querySelectorAll('[id]');
+      const ids = document.querySelectorAll('[class=content]');
+              const length = ids.length;
+
+              // for (let i = 0; i < length; i++) {
+              //   const chart = document.getElementById(ids[i].id);
+              //   console.log(chart, "checkserrrrr")
+              // }
+
+      for (let i = 0; i < length; i++) {
+        const chart = document.getElementById(ids[i].id);
+        console.log(chart, "chartiiiiii")
+
+        // excute this function then exit loop
+        await html2canvas(chart, { scale: 3 }).then(function (canvas) { 
+          doc.addImage(canvas.toDataURL('image/png'), 'JPEG', 10, 50, 200, 150);
+          if (i < (length - 1)) {
+            doc.addPage();
+          }
+        });
+        console.log(i, "iiiiii")
+      }
+
+      // download the pdf with all charts
+              var d = new Date().toISOString().slice(0, 19).replace(/-/g, "");
+        var filename = 'report_' + d + '.pdf';
+      doc.save(filename);
+    
+      this.common.hideLoading()
+
+
+
+
     }
 
     
-   
-  
+  //  search with this query
+  //   iterate pages html2canvas based on array
   
 
   ngOnInit(): void {
@@ -145,6 +277,8 @@ chart: GoogleChartComponent;
 
       google.charts.load('current', { 'packages': ['corechart'] });
       // google.charts.setOnLoadCallback(this.drawChart);
+      this.common.showLoading()
+
       this.drawChart()
       
       this.apiService.postSurvey(this.constantsService.surveyReport, frame).subscribe((succ: any) => {
@@ -178,6 +312,7 @@ chart: GoogleChartComponent;
         console.log(onbj1['data'] + "onbj1")
 
 
+        this.common.hideLoading();
 
       }, err => {
         this.common.hideLoading();
@@ -194,17 +329,32 @@ chart: GoogleChartComponent;
   }
 
 
-  goDownload(format){
-    setTimeout(() => {
-    if(format === 'pdf'){
-      $('.buttons-pdf').click()
-    }
-
-  },1000)
-
-  }
   
   drawChart = () => {
+    this.common.showLoading()
+
+    this.charts.push({
+      title: 'Line Chart',
+      type: 'LineChart',
+      data: [
+        ['Apr 31', 21],
+        ['May 31', 15],
+        ['June 31', 17],
+        ['July 31', 19]
+      ],
+    });
+
+
+    this.charts.push({
+      title: 'Line Chart',
+      type: 'LineChart',
+      data: [
+        ['Apr 21', 21],
+        ['May 21', 15],
+        ['June 21', 17],
+        ['July 21', 19]
+      ],
+    });
 
     this.charts.push({
       title: 'Line Chart',
@@ -234,6 +384,75 @@ chart: GoogleChartComponent;
       }
     });
   
+
+    this.charts.push({
+      title: 'Line Chart',
+      type: 'LineChart',
+      data: [
+        ['April 19', 100],
+        ['May 19', 117],
+        ['June 19', 66],
+        ['July 19', 103]
+      ],
+      options : {
+        vAxis:{
+          minValue: 0,
+          maxValue: 160
+        }
+      }
+    });
+
+    this.charts.push({
+      title: 'Line Chart',
+      type: 'LineChart',
+      data: [
+        ['April 19', 100],
+        ['May 19', 117],
+        ['June 19', 66],
+        ['July 19', 103]
+      ],
+      options : {
+        vAxis:{
+          minValue: 0,
+          maxValue: 160
+        }
+      }
+    });
+
+    this.charts.push({
+      title: 'Line Chart',
+      type: 'LineChart',
+      data: [
+        ['April 19', 100],
+        ['May 19', 117],
+        ['June 19', 66],
+        ['July 19', 103]
+      ],
+      options : {
+        vAxis:{
+          minValue: 0,
+          maxValue: 160
+        }
+      }
+    });
+
+    this.charts.push({
+      title: 'Line Chart',
+      type: 'LineChart',
+      data: [
+        ['April 19', 100],
+        ['May 19', 117],
+        ['June 19', 66],
+        ['July 19', 103]
+      ],
+      options : {
+        vAxis:{
+          minValue: 0,
+          maxValue: 160
+        }
+      }
+    });
+    
     const options = {
       title: 'My Daily Activities',
       legend: {position: 'top'}
@@ -248,6 +467,9 @@ chart: GoogleChartComponent;
 
     }
     // this.chartData  = this.charts
+
+    this.common.hideLoading();
+
   }
 
   ngAfterViewInit() {
